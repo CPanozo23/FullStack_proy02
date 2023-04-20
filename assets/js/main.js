6e12631c8293911299a1d5e24d4789958d1e026e
CPanozo23 <cpanozo@outlook.com>
2023-04-20 14:42:17 -0400

    Delete and edit button functionality and update in routePeople (index)

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,14 +9,12 @@ btnRP.addEventListener('click', optionRP)
 function optionRP(event) {
     event.preventDefault()
     
-    //console.log(people)
     //OPTION TO ADD OR UPDATE DEPENDS ON ID
     let id = parseInt(document.getElementById("id").value)
     if (event.target.innerHTML === "Agregar" && id === 0) {
-        //console.log("Agregar")
         addRP()
     } else if (event.target.innerHTML === "Actualizar") {
-        //updateRP()
+        updateRP()
     } else {
         alert("ERROR")
     }
@@ -25,21 +23,36 @@ function optionRP(event) {
 function addRP() {
     //ADD RP IF NOT EXIST
     const rp = readFormRP()
-    //alert(rp)
     if (rp != undefined) {
         routePeople.push(rp)
         createRowRP(rp)
         
         saveDataLS()
         readDataLS()
-        //readDataLSpeople()
         cleanFormRP()
-        //readDataLSpeople()
         /******************************* */
         
     }
 }
 /*****************[1.2] FORM BUTTON: UPDATE ******************/
+//SÓLO PUEDA CAMBIAR ASIGNACION DE RECORRIDO Y LOS HIJOS
+function updateRP(){
+    let noEmpty = isNotEmpty()
+    if (noEmpty === true) {
+        let rp = createRP(dataForms(), parseInt(dataForms()[4].value), "read")
+        
+        const positionRP = routePeople.findIndex((element) => element.id === rp.id)
+        
+        routePeople[positionRP] = rp
+        const sectionRP = document.getElementById('rpData')
+        sectionRP.innerHTML = ''
+        localStorage.removeItem('routePeople')
+        saveDataLS()
+        readDataLS()
+        cleanFormRP()
+        document.getElementById('staffSelect').disabled=false
+    } 
+}
 /*****************[1.3] FORM BUTTON: GENERAL FUNCTION ******************/
 function dataForms() { //GET ELEMENT BY FORM
     const dataInputForms = [document.getElementById('staffSelect'),
@@ -57,7 +70,7 @@ function readFormRP(event) {
     
     if (noEmpty === true) {
         idAux++
-        return createRP(dataForms(), idAux)
+        return createRP(dataForms(), idAux, "updateHasRoute")//ADD
     }
 }
 
@@ -75,70 +88,44 @@ function isNotEmpty(){
         
     empty===false ? document.getElementById('rpInformation').innerHTML="*Faltan datos" : ''
 
-
-    //FREE SEAT
-    let freeSeat = showFreeSeat()
-    let dataF=dataForms()
-    let seatCount = freeSeat-(1+parseInt(dataF[2].value))
-    if(seatCount<0){
-        document.getElementById('rpInformation').innerHTML+=' *No alcanzan los asientos'
-        empty=false
-    }
-
     return empty    
 }
 
-function searchUpStaff(id){
+function searchUpStaff(id, option){
     const staff = people.find((element)=>element.id===parseInt(id))
     const position = people.findIndex((element)=>element.id===parseInt(id))
-
-    staff.hasRoute === true ? staff.hasRoute=false : staff.hasRoute=true
-    people[parseInt(position)] = staff
-    //UPDATE PEOPLE
-    localStorage.removeItem('people')
-    localStorage.setItem('people', JSON.stringify(people))
-    return staff
-}
-
-function upFreeSeat(id, seat, operation){
-    const route = routes.find((element)=>element.id===id)
-    const position = routes.findIndex((element)=>element.id===id)
-    //alert(seat)
-    //alert(route.freeSeat)
+    switch (option) {
+        case "updateHasRoute":
+            staff.hasRoute === true ? staff.hasRoute=false : staff.hasRoute=true
+            people[parseInt(position)] = staff
+            //UPDATE PEOPLE
+            localStorage.removeItem('people')
+            localStorage.setItem('people', JSON.stringify(people))
+            return staff
+            break
+        case "read":
+            return staff
+            break
+        default:
+            break
+    }
     
-    operation='add' ? route.freeSeat-=seat :''
-    operation='delete' ? route.freeSeat+=seat :''
-    //alert(route.freeSeat)
-
-    routes[parseInt(position)] = route
-    //UPDATE ROUTES
-    localStorage.removeItem('routes')
-    localStorage.setItem('routes', JSON.stringify(routes))
-    return route
+    
 }
 
-function createRP(inputsForm, id) {//CREATE OBJECT
-    const staff= searchUpStaff(inputsForm[0].value)
+function createRP(inputsForm, id, optionStaff) {//CREATE OBJECT
+    const staff = searchUpStaff(inputsForm[0].value, optionStaff)
     dataForms()[1].checked===false ? dataForms()[2].value=0 : ''
 
-    //people.shift()
-    //update Free Seat
-    let seatCount = 1+parseInt(dataForms()[2].value)
-
-    upFreeSeat(parseInt(inputsForm[3].value), seatCount, 'add')
     const route = routes.find((element)=>element.id===parseInt(inputsForm[3].value))
-    //alert(route.freeSeat)
 
     const rp = {
         id: id,
         staff: staff,
-        children: inputsForm[1].value,
+        children: inputsForm[1].checked,
         childrenQ: inputsForm[2].value,
         route: route
     }
-
-    //update freeSeat of bus
-
 
     readDataLS()
     return rp
@@ -171,32 +158,7 @@ function cleanFormRP() {
 document.getElementById('childrenS').addEventListener('click', () => document.getElementById('childrenQ').disabled=false)
 
 document.getElementById('childrenN').addEventListener('click', () => document.getElementById('childrenQ').disabled=true)
-
-//SHOW FREE SEAT
-/*routeSelect.addEventListener('click', ()=>{
-    if(document.getElementById('routeSelect').value!=''){
-        let idSearch= parseInt(document.getElementById('routeSelect').value)
-        const route = routes.find((element) => element.id === idSearch)
-        document.getElementById('freeSeat').innerHTML=route.seat
-    }
-})*/
-
-routeSelect.addEventListener('click', showFreeSeat)
-
-function showFreeSeat(event) {
-    document.getElementById('freeSeat').innerHTML=''
-    if(document.getElementById('routeSelect').value!=''){
-        let idSearch= parseInt(document.getElementById('routeSelect').value)
-        const route = routes.find((element) => element.id === idSearch)
-        document.getElementById('freeSeat').innerHTML=route.freeSeat
-        return route.seat
-    }
-    return 0
-}
-
-
 /*****************[2] LIST ACTION ******************/
-
 sectionRP.addEventListener('click', (event) => {
     event.preventDefault()
 
@@ -205,32 +167,23 @@ sectionRP.addEventListener('click', (event) => {
         deleteRP(event.target.value) //ID
     } else if (event.target.innerHTML === "Editar") {
         editRP(event.target.value)
-    } else { console.log('ERROR: BOTONES') }
+    }
 
 })
 /*****************[2.1] LIST ACTION: DELETE ******************/
-function deleteRP(idSearch) { 
+function deleteRP(idSearch) {
     /*SEARCH OBJECT AND POSITION*/
+    console.log(idSearch)
     const positionRP = routePeople.findIndex((element) => element.id === parseInt(idSearch))
+    console.log(positionRP)
     const rp = routePeople.find((element) => element.id === parseInt(idSearch))
-    
-    /*change free seat */
-    //let seatCount = 1+rp.staff.childrenQ
-    //upFreeSeat(parseInt(rp.route.id), seatCount, 'delete')
-    /*change hasRoute People*/
-    //searchUpStaff(rp.staff.id)
-    
-
-    const staff = people.find((element)=>element.id===parseInt(rp.staff.id))
-    const positionS = people.findIndex((element)=>element.id===parseInt(rp.staff.id))
-
-    staff.hasRoute === true ? staff.hasRoute=false : staff.hasRoute=true
-
-    people[parseInt(positionS)] = staff
-    people.splice(positionS, 1)
-    people.push(staff)
+    console.log(rp)
+    //VERIFICAR DESDE ACÁ
+    //readDataLS()
 
     //UPDATE PEOPLE
+    console.log(people)
+    const staff = searchUpStaff(rp.staff.id, "updateHasRoute")
     localStorage.removeItem('people')
     localStorage.setItem('people', JSON.stringify(people))
 
@@ -244,24 +197,17 @@ function deleteRP(idSearch) {
     saveDataLS()
     readDataLS()
     cleanFormRP()
+
 }
-
-
-
 /*****************[2.2] LIST ACTION: UPDATE ******************/
 function editRP(idSearch) { 
-    /*SEARCH OBJECT AND POSITION*/
-    //const positionRP = routePeople.findIndex((element) => element.id === parseInt(idSearch))
     const rp = routePeople.find((element) => element.id === parseInt(idSearch))
-    //mejorar pq estoy cambiandole el estado
-    const staff=searchUpStaff(rp.staff.id)
-    saveDataLS()
-    readDataLS()
-    const inputForms = dataForms()
-    //no lo recarga pq esta con condicion de q no lo muestre si está como hasRote true, se tendria q quitar esta condición
-    routePeople.hasRoute=false
-    inputForms[0].value=rp.staff.id
-    //checked a la opción que corresponda
+
+    const sectionPeople= document.getElementById('staffSelect')
+    sectionPeople.innerHTML= `<option value="${rp.staff.id}">${rp.staff.run}  ${rp.staff.fName} ${rp.staff.lastName}</option>`
+    document.getElementById('staffSelect').disabled=true
+    
+    inputForms=dataForms()
     if(parseInt(rp.childrenQ)>0){
         inputForms[1].checked=true
         inputForms[2].disabled=false
@@ -271,10 +217,12 @@ function editRP(idSearch) {
         document.getElementById("childrenN").checked=true
         inputForms[2].disabled=true
     }
-    //inputForms[1].value=rp.children //('childrenS') 
+ 
     inputForms[2].value=rp.childrenQ //('childrenQ'),
     inputForms[3].value=rp.route.id //('routeSelect'),
     inputForms[4].value=rp.id //('childrenS')
+    document.getElementById('btnRP').innerHTML="Actualizar"
+    
 }
 /*****************[3.1] LOADED ******************/
 document.addEventListener('DOMContentLoaded', readDataLS())
@@ -285,9 +233,6 @@ function saveDataLSpeople() {
 function saveDataLS() {
     localStorage.setItem('routePeople', JSON.stringify(routePeople))
 }
-
-
-
 function readDataLS(){
     //LOAD ROUTES AND PEOPLE
     readDataLSroutes()
@@ -303,8 +248,6 @@ function readDataLS(){
 
             } else {
                 routePeople.push(element)
-                //console.log('Cantidad: ' + routePeople.length)
-                //console.log(element)
             }
             createRowRP(element)
             idAux = element.id
@@ -314,25 +257,23 @@ function readDataLS(){
 }
 
 function readDataLSroutes(){
+
     const routesRead = JSON.parse(localStorage.getItem('routes'))
     if (routesRead !== null) {
+        
         routes.length=0 //clean route array
         routesRead.sort((a, b) => a.id - b.id) //sort by ID
         const sectionRoute= document.getElementById('routeSelect')
         sectionRoute.innerHTML = '<option value="">Seleccione</option>'
 
-        routesRead.forEach((element) => {//CICLO           
+        routesRead.forEach((element) => {        
 
             if (routes.find((el) => el.id === element.id)) {
 
             } else {
                 routes.push(element)
-                //console.log('Cantidad: ' + routes.length)
-                //console.log(element)
-                //cargar en selection
                 sectionRoute.innerHTML += `<option value="${element.id}">${element.id}- ${element.name}</option>`
             }
-            //createRowRoute(element)
         })
     }
 }
@@ -344,19 +285,14 @@ function readDataLSpeople(){
         peopleRead.sort((a, b) => a.id - b.id) //sort by ID
         const sectionPeople= document.getElementById('staffSelect')
         sectionPeople.innerHTML = `<option value=''>Seleccione</option>`
-        peopleRead.forEach((element) => {//CICLO           
+        peopleRead.forEach((element) => {        
             if (people.find((el) => el.id === element.id)) {
-
             } else {
                 people.push(element)
-                //console.log('Cantidad: ' + people.length)
-                //console.log(element)
-                //cargar en selection
                 if(element.hasRoute === false){
-                    sectionPeople.innerHTML += `<option value="${element.id}">${element.id}- ${element.fName} ${element.lastName} | ${element.hasRoute}</option>`
+                    sectionPeople.innerHTML += `<option value="${element.id}">${element.run}  ${element.fName} ${element.lastName}</option>`
                 }
             }
-            //createRowRoute(element)
         })
     }
 }
